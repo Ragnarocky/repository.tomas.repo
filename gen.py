26c79352a046827baf5b428fcafec36645dea311
Tomáš Pour <pour@top-tech.cz>
2025-12-22 11:03:23 +0100

    Initial commit - Tshare repository with addons v1.2.4 and kontext_menu v1.0.0

---
+++ gen.py
@@ -0,0 +1,21 @@
+﻿import os
+import xml.etree.ElementTree as ET
+import hashlib
+
+addons = ET.Element('addons')
+tree = ET.parse('addon.xml')
+addons.append(tree.getroot())
+km_tree = ET.parse('zip/kontext_menu/addon.xml')
+addons.append(km_tree.getroot())
+
+with open('addons.xml', 'wb') as f:
+    f.write(b'<?xml version=""1.0"" encoding=""UTF-8""?>\n')
+    f.write(ET.tostring(addons, encoding='utf-8'))
+
+with open('addons.xml', 'rb') as f:
+    md5 = hashlib.md5(f.read()).hexdigest()
+
+with open('addons.xml.md5', 'w') as f:
+    f.write(md5)
+
+print('MD5: ' + md5)
